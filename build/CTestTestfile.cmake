fca4f6e973572b83df67c2e66d6332403bd413de
hbtang <jasonhbtang@foxmail.com>
2016-06-03 18:45:27 +0800

    2016.6.3.1

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thb/demo-g2o
-# Build directory: /home/thb/demo-g2o/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/thb/demo-g2o/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
